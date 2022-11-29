c86118aad71d87f151d35cc8ecf62c5627cbb134
scherenhaenden <scherenhaenden@gmail.com>
2022-11-29 18:14:57 +0100

    Add required

--- IdentityService.DataAccess.Database/Persistance/Domain/UserCompact.cs
+++ IdentityService.DataAccess.Database/Persistance/Domain/UserCompact.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using IdentityService.DataAccess.Database.Core.BaseDomain;
 using IdentityService.DataAccess.Database.Core.Domain;
 
@@ -5,7 +6,12 @@ namespace IdentityService.DataAccess.Database.Persistance.Domain;
 
 public class UserCompact : BaseEntity, IUserCompact
 {
+    [Required]
     public string Email { get; set; } = null!;
+    
+    [Required]
     public string Username { get; set; } = null!;
+    
+    [Required]
     public string Password { get; set; }
 }

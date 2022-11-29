6f654122fbde8c2daeaaa888105cbd89e2a542c4
scherenhaenden <scherenhaenden@gmail.com>
2022-11-29 18:14:26 +0100

    Update name

--- IdentityService.DataAccess.Database/Persistance/Domain/Claim.cs
+++ IdentityService.DataAccess.Database/Persistance/Domain/Claim.cs
@@ -3,7 +3,7 @@ using IdentityService.DataAccess.Database.Core.Domain;
 
 namespace IdentityService.DataAccess.Database.Persistance.Domain;
 
-public class ClaimBase : BaseEntity, IClaimBase
+public class Claim : BaseEntity, IClaim
 {
     public string ClaimType { get; set; } = null!;
     public string ClaimValue { get; set; } = null!;

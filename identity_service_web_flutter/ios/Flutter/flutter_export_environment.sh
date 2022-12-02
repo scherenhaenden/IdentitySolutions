509dd61f889d3d447ae89ae5577f250e8fed4153
scherenhaenden <scherenhaenden@gmail.com>
2022-12-02 10:16:00 +0100

    add the rest of flutter

---
+++ identity_service_web_flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/edwardflores/Projects/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/edwardflores/Projects/Development/IdentitySolutions/identity_service_web_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/edwardflores/Projects/Development/IdentitySolutions/identity_service_web_flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/edwardflores/Projects/Development/IdentitySolutions/identity_service_web_flutter/.dart_tool/package_config.json"

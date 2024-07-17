870842ac7b316464a1731e99883b0bc776f1a2bc
scherenhaenden <scherenhaenden@gmail.com>
2024-07-17 16:55:28 +0200

    chore: Refactor IdentityServiceInstallationDataAccessService

--- IdentityService.DataAccess/Services/Installation/IdentityServiceInstallationDataAccessService.cs
+++ IdentityService.DataAccess/Services/Installation/IdentityServiceInstallationDataAccessService.cs
@@ -65,4 +65,63 @@ namespace IdentityService.DataAccess.Services.Installation
             return user;
         }
     }
+    
+    public class IdentityServiceInstallationDataAccessServiceV2: IIdentityServiceInstallationDataAccessService
+    {
+        
+        
+        
+        public SystemUser? Install(InstallationModel model)
+        {
+            
+            
+            var errors = model.ValidateWithMessage();
+            // need to add logs
+            if (errors.Any())
+            {
+                return null;
+            }
+            
+            
+            var context = GenerateContext(model);
+            
+            IUnityOfWorkGlobal unityOfWorkGlobal = new UnityOfWorkGlobal(context);
+
+            SystemUser user = new SystemUser();
+
+            user.Email = model.AdminEmail;
+            user.Password = model.AdminPassword;
+            user.Username = model.AdminUserName;
+            user.FirstName = model.AdminFirstName;
+            user.LastName = model.AdminLastName;
+
+
+
+            unityOfWorkGlobal.GlobalUsers.Add(user);
+            unityOfWorkGlobal.Save();
+            return user;
+        }
+        
+        private IdentityContextGlobal GenerateContext(InstallationModel model) {
+            
+            IContextManager contextManager = new ContextManager();
+            var typeOfDataBae = Enum.GetName(model.DatabaseType).ToLower();
+            var context = contextManager.GenerateGlobalContext(typeOfDataBae, model.ConnectionString);
+            return context;
+        }
+
+        public SystemUser Install_Step_AddAminUser(InstallationModel model, IUnityOfWorkGlobal unityOfWorkGlobal)
+        {
+            SystemUser user = new SystemUser();
+
+            user.Email = model.AdminEmail;
+            user.Password = model.AdminPassword;
+            user.Username = model.AdminUserName;
+
+
+            unityOfWorkGlobal.GlobalUsers.Add(user);
+            unityOfWorkGlobal.Save();
+            return user;
+        }
+    }
 }
